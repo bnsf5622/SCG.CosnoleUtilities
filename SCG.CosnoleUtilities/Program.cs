e9b92c1296abc8200dba4281c68561396d3058b5
chuck francis <chuck_francis10@yahoo.com>
2020-10-08 13:19:36 -0400

    Add project files.

---
+++ SCG.CosnoleUtilities/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCG.ConsoleUtilities
+{
+    public class UserInput
+    {
+        public int GetIntFromUSer(string prompt)
+        {
+            string input;
+            int outrput;
+
+            while (true)
+            {
+                Console.Write(prompt)
+                input = Console.ReadLine();
+
+                if (int.TryParse(input, out output))
+                    return Outputter;
+
+                Console.Write("That was not a valid integer... press any key to continue");
+                Console.ReadKey();
+            }
+        }
+    }
+}
